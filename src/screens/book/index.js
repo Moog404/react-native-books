7ec2f5ad1a0dfa9553f0116ab81ae90c4d57a351
Eang Ly <eangly.eng@gmail.com>
2021-04-04 19:39:34 +0200

    feat: added active storage books like

--- src/screens/book/index.js
+++ src/screens/book/index.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import Header from "../../composants/Header";
+import {getData, storeData} from "../../../utils/StoreManager";
 
 export default function book({route, navigation}) {
     const { item } = route.params ;
+    const [like, setLike] = useState(false);
+
+    useEffect(()=>{
+        async function getLike() {
+            let like = await getData('@book_like_'+item.id);
+            setLike(like);
+        }
+            getLike();
+
+    }, []);
+
+    const isLike = async (like) => {
+        const key = '@book_like_' + item.id ;
+        await storeData(key, like) ;
+        setLike(like);
+    };
 
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
             <Header />
+            {item.fields["Cover Photo"] !== undefined && item.fields["Cover Photo"].length > 0 && <Image source={{uri: item.fields["Cover Photo"][0]["url"]}} style={styles.imageSize} />}
             <View style={{ width: '100%', height: '90%'}}>
+                <TouchableOpacity onPress={() => { isLike(!like)}}><Text>{like ? 'Ne plus mettre en favoris' : 'Mettre en favoris'}</Text></TouchableOpacity>
                 <Text>{item.fields["Name"]}</Text>
                 <Text>{item.fields["Synopsis"]}</Text>
                 <Text>Auteur : {item.fields["Author"]}</Text>
-                {item.fields["Cover Photo"] !== undefined && item.fields["Cover Photo"].length > 0 && <Image source={{uri: item.fields["Cover Photo"][0]["url"]}} style={{ width: '100%', height: '100%'}} />}
             </View>
         </View>
     );
--- src/screens/book/styles.js
+++ src/screens/book/styles.js
@@ -3,12 +3,16 @@ import {StyleSheet} from "react-native";
 export default styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red',
+        backgroundColor: 'green',
         alignItems: 'center',
         justifyContent: 'center',
     },
     button: {
         color: "#65abf6",
         fontSize: 20
+    },
+    imageSize: {
+        width: 200,
+        height: 400,
     }
 });

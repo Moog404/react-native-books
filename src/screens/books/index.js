42d0a041203166e2e11e304d59d6d200e8dd7ddc
Eang Ly <eangly.eng@gmail.com>
2021-05-28 16:59:07 +0200

    feat: added searchbar

--- src/screens/books/index.js
+++ src/screens/books/index.js
@@ -4,23 +4,39 @@ import { StyleSheet, Text, View, Button, Image, TouchableOpacity, TouchableHighl
 import styles from './styles';
 import Book from "../../composants/Book";
 import getBooks from "../../../models/books";
+import {SearchBar} from "react-native-elements";
 
 export default function books({route, navigation}) {
     const [ books, setBooks] = useState([]);
+    const [ dataBooks, setDataBooks] = useState([]);
+    const [ input, setInput] = useState([]);
 
     useEffect(() => {
         async function fetchData()  {
             let books = await getBooks() ;
             setBooks(books);
+            setDataBooks(books);
         }
         fetchData() ;
     }, []);
 
+    const updateInput = async (input) =>{
+        const filtered = dataBooks.filter(item => {
+            return item.fields['Name'].toLowerCase().includes(input.toLowerCase())
+        })
+        setInput(input);
+        setBooks(filtered);
+    }
+
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-
             <View style={{ width: '100%', height: '95%'}}>
+                <SearchBar
+                    placeholder="Rechercher"
+                    onChangeText={updateInput}
+                    value={input}
+                />
                 <FlatList
                     data={books}
                     renderItem={({ item }) => (
